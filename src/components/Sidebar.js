6e2288762f92cf32e90cbc582742a3b6b98610bf
Zhanaaql <zulfahanaaqliyah@gmail.com>
2023-11-08 21:36:46 +0700

    sidebar responsive

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,11 +15,11 @@ const Sidebar = () => {
     <div
       className={`${
         isOpen ? "right-0" : "-right-full"
-      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px] overflow-auto`}
     >
       <div className="flex items-center justify-between py-6 border-b">
         <div className="uppercase text-sm font-semibold">
-          Shopping Bag ({itemAmount})
+          Shopping Cart ({itemAmount})
         </div>
         <div
           onClick={handleClose}
@@ -49,12 +49,6 @@ const Sidebar = () => {
             <FiTrash2 />
           </div>
         </div>
-        <Link
-          to={"/"}
-          className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium"
-        >
-          View cart
-        </Link>
         <Link
           to={"/"}
           className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium"
